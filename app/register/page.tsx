5e92617e4f0b845ad1bffe6bc14d97fd87a90821
sleeptd2 <alvarojuniorctb@hotmail.com>
2024-01-05 17:13:47 -0300

    feat: cryptografar as senhas e ajuste no cadastro do usuário.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,46 +1,88 @@
 "use client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
-import React, { useState } from "react";
+import Link from "next/link";
+import { useState } from "react";
 
-function RegisterPage() {
+export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function handleFormSubmit(ev) {
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+  async function handleFormSubmit(ev) {
     ev.preventDefault();
-    fetch("/api/register", {
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
+    setCreatingUser(false);
   }
   return (
     <section className="mt-8">
-      <h1 className="text-center text-primary text-4xl mb-4">Cadastro</h1>
-
+      <h1 className="text-center text-primary text-4xl mb-4">Cadastrar</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          Usuário cadastrado
+          <br />
+          Agora você pode fazer o {""}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          E-mail já cadastrado
+          <br />
+          Por favor tente outro e-mail ou faça o{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-        <label>Email</label>
+        <label className="text-black text-md font-semibold ml-2">Email</label>
         <input
-          type="text"
+          type="email"
           value={email}
+          disabled={creatingUser}
           onChange={(ev) => setEmail(ev.target.value)}
         />
-        <label>Password</label>
+        <label className="text-black text-md font-semibold ml-2">Senha</label>
         <input
-          type="text"
+          type="password"
           value={password}
+          disabled={creatingUser}
           onChange={(ev) => setPassword(ev.target.value)}
         />
-        <button type="submit">Cadastrar</button>
-        <div className="my-4 text-center text-gray-500">
-          ou utilize outro método.
-        </div>
-        <button className="flex gap-4 justify-center">
-          <Image src="/google.png" alt="google-login" width={24} height={24} />
+        <button type="submit" disabled={creatingUser}>
+          Cadastrar
+        </button>
+        <div className="my-4 text-center text-gray-500">ou</div>
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="flex gap-4 justify-center"
+        >
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
           Continue with google
         </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Ja tem uma conta?
+          <Link className="underline pl-2" href={"/login"}>
+            Faça o login &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
 }
-
-export default RegisterPage;
